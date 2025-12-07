d002a823b0098eb9c4d52f177441af0dfdded4b4
Feldwor <BoQsc@users.noreply.github.com>
2025-12-07 09:22:27 +0200

    Add options menu

--- main_menu.gd
+++ main_menu.gd
@@ -2,20 +2,53 @@ extends Control
 
 @onready var click_sound = $ClickSoundPlayer
 
+# Preload the options menu scene
+var options_scene = preload("res://options_menu.tscn")
+
+# Define button paths
+@onready var steam_btn = $MainMenu/MarginContainer/TextureButton
+@onready var play_btn = $MainMenu/TextureButton5/HBoxContainer/MarginContainer/TextureButton
+@onready var options_btn = $MainMenu/TextureButton5/HBoxContainer/MarginContainer2/TextureButton2
+@onready var credits_btn = $MainMenu/TextureButton5/HBoxContainer/MarginContainer3/TextureButton3
+@onready var quit_btn = $MainMenu/TextureButton5/HBoxContainer/MarginContainer4/TextureButton4
+@onready var main_menu_container = $MainMenu
+
 func _ready():
-	# Connect all buttons to the click sound
-	var buttons = [
-		$MainMenu/MarginContainer/TextureButton,
-		$MainMenu/TextureButton5/HBoxContainer/MarginContainer/TextureButton,
-		$MainMenu/TextureButton5/HBoxContainer/MarginContainer2/TextureButton2,
-		$MainMenu/TextureButton5/HBoxContainer/MarginContainer3/TextureButton3,
-		$MainMenu/TextureButton5/HBoxContainer/MarginContainer4/TextureButton4
-	]
+	# Connect buttons to their specific functions
+	if play_btn:
+		play_btn.pressed.connect(_on_play_pressed)
+	if options_btn:
+		options_btn.pressed.connect(_on_options_pressed)
+	if quit_btn:
+		quit_btn.pressed.connect(_on_quit_pressed)
 	
+	# Connect all buttons to sound effect
+	var buttons = [steam_btn, play_btn, options_btn, credits_btn, quit_btn]
 	for btn in buttons:
 		if btn:
-			btn.pressed.connect(_on_button_pressed)
+			btn.pressed.connect(_play_click_sound)
 
-func _on_button_pressed():
+func _play_click_sound():
 	if click_sound:
 		click_sound.play()
+
+func _on_play_pressed():
+	print("Play pressed! - Add scene change logic here")
+	# Example: get_tree().change_scene_to_file("res://game_scene.tscn")
+
+func _on_options_pressed():
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	
+	# Listen for when the back button is pressed in the options menu
+	options_instance.back_pressed.connect(_on_options_closed)
+	
+	# Hide the main menu while options are open
+	main_menu_container.visible = false
+
+func _on_options_closed():
+	# Show the main menu again
+	main_menu_container.visible = true
+
+func _on_quit_pressed():
+	get_tree().quit()
